4764bdd02bab5b8ec5e6558609ea1e91b61789f1
DownloadFirefox <61615730+uimaxbai@users.noreply.github.com>
2023-11-09 10:40:35 +0000

    g

---
+++ school-lost-property/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div>
+	<h1>School Lost Property is currently in development</h1>
+</div>
+
+<style lang="scss">
+	@import '../../node_modules/bootstrap/scss/bootstrap.scss';
+	* {
+		font-family: 'Lato';
+	}
+	div {
+		h1 {
+			text-align: center;
+		}
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
